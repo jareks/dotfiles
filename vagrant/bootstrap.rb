508967064240fb4a931df33ee33c3eb03de4b9b6
Jarosław Skrzypek <skrzypek.jarek@gmail.com>
2013-02-23 19:38:57 +0100

    added installing virtualbox to bootstraping vagrant

--- vagrant/bootstrap.rb
+++ vagrant/bootstrap.rb
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# install virtualbox
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian precise contrib" >> /etc/apt/sources.list'
+sudo apt-get update && sudo apt-get install virtualbox-4.2
+
 gem install vagrant
 
