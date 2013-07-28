cee5a29d4f60952dabe9a2b8246bc1288a4e9775
Jarosław Skrzypek <skrzypek.jarek@gmail.com>
2013-07-28 14:59:15 +0200

    postgres and vagrant info

--- vagrant/bootstrap.rb
+++ vagrant/bootstrap.rb
@@ -5,5 +5,7 @@ wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | s
 sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian precise contrib" >> /etc/apt/sources.list'
 sudo apt-get update && sudo apt-get install virtualbox-4.2
 
-gem install vagrant
+cd /tmp
+wget http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_x86_64.deb
+sudo dpkg -i vagrant_1.2.2_x86_64.deb
 
